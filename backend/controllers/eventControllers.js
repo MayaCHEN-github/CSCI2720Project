0e377506bbe14ab2af8106203435a4dda34f1e45
Vera Ma <86516036+VeraIsMay@users.noreply.github.com>
2023-12-07 19:57:59 +0800

    Add files via upload

---
+++ backend/controllers/eventControllers.js
@@ -0,0 +1,15 @@
+//model
+const { Venue, Event } = require('../models/Models')
+const mongoose = require('mongoose');
+
+
+// function 4: get all event
+const getEvents = async (req, res) => {
+    const result = await Event.find({});
+    res.status(200).json(result);
+}
+
+// export function
+module.exports = {
+    getEvents
+}
