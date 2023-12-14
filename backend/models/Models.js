be9e0b5cfc3ea4f937036f6d88f1a02b8acea393
Vera Ma <86516036+VeraIsMay@users.noreply.github.com>
2023-12-14 15:05:19 +0800

    Add files via upload

---
+++ backend/models/Models.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const venueSchema = new Schema({
+    venueId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    latitude: {
+        type: Number
+    },
+    longitude: {
+        type: Number
+    }
+});
+
+const eventSchema = new Schema({
+    eventId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    date: { type: String },
+    description: { type: String },
+    presenter: { type: String },
+    price: { type: String },
+    venueId: { type: Number }
+});
+
+const Event = mongoose.model('Event', eventSchema);
+const Venue = mongoose.model('Venue', venueSchema);
+
+module.exports = {
+    Event,
+    Venue
+};
---
+++ backend/routes/venues.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {
+    getVenues,
+    getByKeyword,
+    getById,
+    addComment,
+    // getAllVenues
+} = require('../controllers/venueControllers')
+
+const router = express.Router();
+
+// function 1: get 10 venus with at least 3 events.
+router.get('/', getVenues)
+// router.get('/all', getAllVenues)
+
+// function 2: search keyword in venue name
+router.get('/key/:key', getByKeyword)
+
+// function 3: venue info + comments on it
+router.get('/:id', getById)
+
+// comments part
+// add comment for venue id for user name
+router.post('/comment', addComment)
+
+module.exports = router;
